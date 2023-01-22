b3d81654bf40306a031f1b163fcbfc89194e7852
Ahmed Idrissi <83851898+ahmedidrissi@users.noreply.github.com>
2023-01-23 00:08:48 +0100

    add second theme

--- assets/js/switch.js
+++ assets/js/switch.js
@@ -1,8 +1,22 @@
-function switch_light() {
-    link = document.getElementById("style_link");
-    link.setAttribute("href", "assets/css/style_light.css")
-}
-function switch_dark() {
-    link = document.getElementById("style_link");
-    link.setAttribute("href", "assets/css/style_dark.css")
+// Get the root element
+var r = document.querySelector(':root');
+
+function Theme1() {
+    link = document.getElementById("hello-world");
+    link.setAttribute("src", "assets/img/hello_world_1.png")
+    var rs = getComputedStyle(r);
+    r.style.setProperty('--color-1', rs.getPropertyValue('--theme1-color-1'));
+    r.style.setProperty('--color-2', rs.getPropertyValue('--theme1-color-2'));
+    r.style.setProperty('--color-3', rs.getPropertyValue('--theme1-color-3'));
+    r.style.setProperty('--color-4', rs.getPropertyValue('--theme1-color-4'));
+  }
+
+function Theme2() {
+  link = document.getElementById("hello-world");
+  link.setAttribute("src", "assets/img/hello_world_2.png")
+  var rs = getComputedStyle(r);
+  r.style.setProperty('--color-1', rs.getPropertyValue('--theme2-color-1'));
+  r.style.setProperty('--color-2', rs.getPropertyValue('--theme2-color-2'));
+  r.style.setProperty('--color-3', rs.getPropertyValue('--theme2-color-3'));
+  r.style.setProperty('--color-4', rs.getPropertyValue('--theme2-color-4'));
 }
