c7429c6ed9334c8477c45c1ea09a4391a5151dad
ahmedidrissi <idrissiahmed2002@gmail.com>
2023-07-10 16:09:58 +0100

    create music.js

---
+++ assets/js/music.js
@@ -0,0 +1,19 @@
+/*-------------------------------------------
+  Music button
+-------------------------------------------*/
+function playmusic () {
+    let musicbtn = document.querySelector('.music-btn');
+    let musicicon = document.querySelector('.music-icon');
+    let mymusic = document.getElementById('mymusic');
+    mymusic.volume = 0.3;
+
+    if (musicbtn.classList.contains('stop')) {
+        musicbtn.classList.replace('stop', 'play');
+        musicicon.classList.replace('bi-play', 'bi-pause');
+        mymusic.play();
+    } else {
+        musicbtn.classList.replace('play', 'stop');
+        musicicon.classList.replace('bi-pause', 'bi-play');
+        mymusic.pause();
+    }
+}
